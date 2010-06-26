2d89d770d697767bbe1937ad0a27e5d94387d8c4
Emily Leathers <peppermintdove@gmail.com>
2010-06-26 00:26:06 -0700

    add brand new rails3 project sewit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '7eab7946bd575af858d0752d718ae5c7aa0d4fd9ffc268d60e08d1a9a23eaf1ec0b344949d4c419a744153ca08dbf36bffcd701ebba372f5d68b26951b017f45'
